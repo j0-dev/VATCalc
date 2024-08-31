b5e4642385af44d14b0ed16c59e7b0e19527fab9
Josh Birch <2312292@chester.ac.uk>
2024-08-31 11:28:34 +0100

    Add core logic and error handling
    Implemented the VAT calculation function and added error handling for input. Includes checks for decimals, non-negatives, and adds an upper limit.

--- main.py
+++ main.py
@@ -16,14 +16,44 @@ def vat(net):
     gross = net + amount
     return gross
 
+# # Console test
+# if __name__ == "__main__":
+#     net = float(input("Net amount (excluding VAT): £"))
+#     gross = vat(net)
+#     print(f"Gross amount (including VAT): £{gross:.2f}")
 
-# Test
-if __name__ == "__main__":
-    net = float(input("Net amount (excluding VAT): £"))
-    print(f"Gross amount (including VAT): £{gross:.2f}")
+# Check for decimal
+def decimal(value, max=2):
+    try:
+        _, pence = value.split(".")
+        return len(pence) <= max
+    except ValueError:
+        return True
+    
+# Calculation and error handling
+def calc(event=None):
+    try:
+        # Retrieve input (!update for GUI later)
+        net = int(input("Net amount (excluding VAT): £"))
 
+        # Check for decimal
+        if not decimal(net):
+            raise ValueError("Pence amount should not have more than two decimal places.")
+        
+        # Convert input (!for later)
+        net = float(net)
 
-# x = int(input("Net amount (Excluding VAT(£)): "))
+        # Checks for non-negative
+        if net < 0:
+            raise ValueError("Net value must be non-negative.")
+        
+        # Upper limit:
+        if net > 1000000000:
+            raise ValueError("Net value must be less than £1b.")
+        
+        # Calculation proper
+        result = vat(net)
+        print(f"Gross amount (including VAT): £{result:.2f}")
 
-# result = vat(x)
-# print(f"The gross amount (Including VAT(£)): {result:.2f}")
+    except ValueError as error:
+        print(f"Error: {error}")
